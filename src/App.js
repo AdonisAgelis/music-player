d8081eef7ca81b42e5ea29e5f141f19a525f2348
Sweet_Scientist <64701309+AdonisAgelis@users.noreply.github.com>
2021-09-16 18:47:43 +0300

    Play / Pause functionality

--- src/App.js
+++ src/App.js
@@ -7,13 +7,19 @@ import Song from './components/Song';
 import data from './util';
 
 const App = () => {
+  // State
   const [songs, setSongs] = useState(data());
   const [currentSong, setCurrentSong] = useState(songs[0]);
+  const [isPlaying, setIsPlaying] = useState(false);
 
   return (
     <div className="App">
       <Song currentSong={currentSong} />
-      <Player />
+      <Player
+        currentSong={currentSong}
+        isPlaying={isPlaying}
+        setIsPlaying={setIsPlaying}
+      />
     </div>
   );
 };
