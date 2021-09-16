3cde0ef067de8ccab6ccd898d5e14bdb0f118759
Sweet_Scientist <64701309+AdonisAgelis@users.noreply.github.com>
2021-09-16 18:21:53 +0300

    Adding State and Styles

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import React from 'react';
-import styles from './styles/app.scss';
+import React, { useState } from 'react';
+import './styles/app.scss';
 
 import Player from './components/Player';
 import Song from './components/Song';
 
+import data from './util';
+
 const App = () => {
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+
   return (
     <div className="App">
-      <Song />
+      <Song currentSong={currentSong} />
       <Player />
     </div>
   );
